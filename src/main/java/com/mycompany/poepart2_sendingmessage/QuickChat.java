546ec08271ec0778622c8df70892a2167c38850d
deepl <deepl@OmphileMoiloa>
2026-05-24 04:51:18 +0200

    linking my project to Github CREATING METHODS rename the project file to QuickChat	 because it had errors creating more methods new class called Message linking to github again!!

--- src/main/java/com/mycompany/poepart2_sendingmessage/QuickChat.java
+++ src/main/java/com/mycompany/poepart2_sendingmessage/QuickChat.java
@@ -12,9 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class PoePart2_sendingMessage {
-    // A shared list that stores all Message objects created while the program runs
-    public static List<Message> messages =new ArrayList<>();
+public class QuickChat {
+    // A shared list that stores all QuickChat objects created while the program runs
+    public static List<QuickChat> messages =new ArrayList<>();
     public static int totalMessages = 0;
     
     //checking message ID(less than and equal to 1o charcters
@@ -33,7 +33,7 @@ public class PoePart2_sendingMessage {
          }
         }
     // creating a message Hash
-    //This method generates a unique “Message Hash”, a short code that represents the message.
+    //This method generates a unique “QuickChat Hash”, a short code that represents the message.
     //It combines parts of the message ID, the number of messages sent, and the first/last words of the message.
     
     public static String createMessageHash(String messageID, int numMessages, String message) {
@@ -46,7 +46,7 @@ public class PoePart2_sendingMessage {
         return messageID.substring(0, 2) + ":" + numMessages + ":" + firstWord + lastWord;
     }
     //send_store_disregard message
-    public static String sendMessage(Message msg, int option){
+    public static String sendMessage(QuickChat msg, int option){
         switch(option) {
             //send message
             case 1:
@@ -65,7 +65,65 @@ public class PoePart2_sendingMessage {
         }
     }
     
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    //Print messages
+    public static void printMessages(){
+        //for loop
+        for (QuickChat msg : messages) {
+            System.out.println(msg);
+        }
     }
+    
+    //return total messages
+    public static int returnTotalMessages(){
+        return totalMessages;
+    }
+    // Store QuickChat in JSON
+    //did some research at w3school
+    
+    public static void storeMessageJSON(Message msg) {
+        // Placeholder: use a JSON library like Gson
+        // Example: new Gson().toJson(msg);
+    }
+    
+    // main program loop
+    public static void main(String[] args) {
+        //scanner name is chat
+        Scanner chat = new Scanner(System.in);
+        //message when after logging in thr chatapp
+        System.out.println("Welcome to Quickchat");
+        
+        // tells the user to type how many messages they want to send.
+        System.out.println("Enter number of messages: ");
+        int num =chat.nextInt();
+        chat.nextLine();
+        
+        //for loop, goes with createMessageHash
+        for (int i = 0; i < num; i++){
+            System.out.println("Enter Message ID: ");
+            String id = chat.nextLine();
+            
+            System.out.println("Enter Recipient Number: ");
+            String recipient = chat.nextLine();
+            
+            System.out.println("Enter Message: ");
+            String text = chat.nextLine();
+            
+            Message msg = new Message(id, recipient, text);
+            
+            //ask the user what to do with message
+            System.out.println("Choose option: 1=Send, 2=Disregard/delete 3=Store");
+            int option = chat.nextInt();
+            chat.nextLine(); //clear buffer
+            
+            //Call sendMessage and show result
+            String results = sendMessage(msg, option);
+            System.out.println(results);
+        }
+        
+        //show total messages sent at the end
+        System.out.println("Total messages sent: " + totalMessages);
+        
+    }
+    
+   
 }
